71d01b61bada1511e7a3987bd96cb5fe14519d5a
im linux <imlinux68@gmail.com>
2023-03-26 14:17:59 +0300

    py-comit1

---
+++ data/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to super calendar')
+
+year = int(input('Enter an year: '))
+month = int(input('Enter a month: '))
+
+print(calendar.month(year,month))
+
+print('Good bye')
